ad58d8fab85288005f2de1cd9744a0c9a3976ca3
Peter <ptk726@gmail.com>
2018-01-14 01:33:33 -0500

    Auth routes and password digest added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   resources :roles
   resources :movies
   resources :actors
+
+  get 'signup'  => 'users#new'
+  resources :users
+
+  get '/login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
