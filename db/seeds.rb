b11035e89635d373833837eda71b01f55d4851c3
Vernon Reis <32598819+venom9122@users.noreply.github.com>
2018-01-14 16:45:36 -0800

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.create(
+  title: 'Die Hard',
+  year: 1988,
+  rating: 'R',
+  released: '7/20/1988',
+  runtime: 132,
+)
+
+Movie.create(
+  title: 'John Wick',
+  year: 2014,
+  rating: 'R',
+  released: '10/24/1988',
+  runtime: 132,
+)
+
+Movie.create(
+  title: 'Jumanji',
+  year: 1995,
+  rating: 'PG',
+  released: '12/15/1995',
+  runtime: 104,
+)
